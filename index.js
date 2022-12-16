57335d36a5d02cdfa5cada41f003498ad608e763
Pierce <pierceevans@me.com>
2022-12-16 15:14:58 -0600

    commit tree de9c925e6ddeb5fd0e71b6b2abb5890685ea3ee8

---
+++ index.js
@@ -0,0 +1,6 @@
+const divide = (parameter1, parameter2) => parameter1 / parameter2;
+divide(2,8);
+const square = parameter1 => parameter1 * parameter1;
+square(4);
+const add = (parameter1, parameter2) => parameter1 + parameter2;
+add(2,3);
